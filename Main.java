61c65606d27c4233ca70b89322b7099417967d02
nbakshi15 <nirali.bakshi@yahoo.com>
2016-10-10 10:29:13 -0400

    Sup

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+
+        while (
+
+        System.out.println("Hi! What is your name?");
+        String name = input.next();
+        System.out.println("Thats pretty! Whats your favorite color?");
+        String color = input.next();
+        System.out.println();
+
+        System.out.println();
+    }
+}
---
+++ Random.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by nb035 on 10/10/16.
+ */
+public class Random {
+    public number (String n)
+    {
+        name = n;
+    }
+}
